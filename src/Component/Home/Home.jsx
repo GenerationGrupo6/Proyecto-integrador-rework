7112b3d94d3552a78696c2da8f07abc9650a0590
David.vicencio <david.vichipoms@gmail.com>
2023-11-08 11:26:13 -0300

    responsive luna

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -7,13 +7,11 @@ function Home() {
     <div className="First-Container-Home-Page">
       <main className="Body-Home-Page">
         <section className="Ilustration-Section-Home-Page">
-          <figure className="Full-Ilustation-Home-Page">
-            <header className="Header-Home-Page">
-              <h3 className="We-Feed-The-Change"> ALIMENTEMOS EL CAMBIO</h3>
-              <h2 className="Together">¡JUNTOS!</h2>
-            </header>
+          <header className="Header-Home-Page">
+            <h3 className="We-Feed-The-Change"> ALIMENTEMOS EL CAMBIO</h3>
+            <h2 className="Together">¡JUNTOS!</h2>
             <DonationButton />
-          </figure>
+          </header>
         </section>
         <section className="Infortation-Cloud-Section-Home-Page">
           <InformationCloud />
@@ -23,4 +21,4 @@ function Home() {
   );
 }
 
-export {Home}
+export { Home };
