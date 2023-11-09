f92e36701ee75aae464dd45d3dc0080a7f3ecc7b
Felipe <Felipefriasmorales@gmail.com>
2023-11-09 17:21:57 -0300

    login sin nav

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/Logo-sin-fondo.png'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+        <Link to='/'>
+            <img id='LogoNav' src={logo} alt="LogoCanastasDeVida" />
+        </Link>
   )
 }
