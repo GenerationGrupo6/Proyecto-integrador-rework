b76e3b46bd1a17e04cf5ae5d5564b55a4c5b65fe
Theredrevenge23 <gonzalo.quiroz.parra@gmail.com>
2023-11-09 17:58:31 -0300

    cambios 9/11/2023
    pulí un poco la pagina de donaciones ,le saque un fondo a una imagen y agregué un link al botón de donaciones.

--- src/Component/DonationButton/DonationButton.jsx
+++ src/Component/DonationButton/DonationButton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./DonationButton.css";
+import { Link } from "react-router-dom";
 
 const DonationButton = () => {
-  return <button className="Donation-Button">Haz tu Aporte Aquí</button>;
+  return (
+      <Link to='/unete-al-cambio'>
+      <button className="Donation-Button" >Haz tu Aporte Aquí</button>;
+      </Link>
+  )
 };
 
 export { DonationButton };
