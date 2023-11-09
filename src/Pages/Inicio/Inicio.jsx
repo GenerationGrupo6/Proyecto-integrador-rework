be53206f50f9ad3f12ee2eda0b4a61a5343f826c
lunapazfernanda <luna.gamero@gmail.com>
2023-11-09 15:13:35 -0300

    Arreglos

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { SectionCards } from "../../Component/SectionCards/SectionCards.jsx";
 import { Home } from "../../Component/Home/Home.jsx";
-import { Home2 } from "../../Component/Home/Home2.jsx";
 
 function Inicio() {
   return (
     <>
       <Home />
-      <Home2 />
       <SectionCards header="Nuestras Historias" />
     </>
   );
