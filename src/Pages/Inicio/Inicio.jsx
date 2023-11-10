e73898bd5e388ca78b2ade3c57ac7aaecb6a4455
moirajm <mmunozm3@icloud.com>
2023-11-10 10:44:02 -0300

    help

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SectionCards } from "../../Component/SectionCards/SectionCards.jsx";
 import { Home } from "../../Component/Home/Home.jsx";
+import { Mapa } from "../../Component/Mapa/Mapa.jsx";
 
 function Inicio() {
   return (
