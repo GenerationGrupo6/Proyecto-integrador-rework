47c7a124f521388b91d59374b5b2b99a28297488
lunapazfernanda <luna.gamero@gmail.com>
2023-11-23 03:23:29 -0300

    vamos que vamos

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -3,6 +3,7 @@ import { SectionCards } from "../../Component/SectionCards/SectionCards.jsx";
 import { Home } from "../../Component/Home/Home.jsx";
 import { Mapa } from "../../Component/Mapa/Mapa.jsx";
 import { VenDonaciones } from "../../Component/VenDonaciones/VenDonaciones.jsx";
+import { Subscription } from "../../Component/Subscription/subscription.jsx"
 
 function Inicio() {
   return (
@@ -10,7 +11,8 @@ function Inicio() {
       <Home />
       <SectionCards header="Nuestras Historias" />
       <Mapa/>
-      <VenDonaciones/>
+      <VenDonaciones />
+      <Subscription/>
     </>
   );
 }
