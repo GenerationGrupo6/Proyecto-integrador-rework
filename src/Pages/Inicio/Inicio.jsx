5474564852887a5ba1583fc0f0b9003e72e79a85
moirajm <mmunozm3@icloud.com>
2023-11-23 04:08:11 -0300

    cambios 2

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -3,6 +3,7 @@ import { SectionCards } from "../../Component/SectionCards/SectionCards.jsx";
 import { Home } from "../../Component/Home/Home.jsx";
 import { Mapa } from "../../Component/Mapa/Mapa.jsx";
 import { VenDonaciones } from "../../Component/VenDonaciones/VenDonaciones.jsx";
+import { Subscription } from "../../Component/Subscription/subscription.jsx"
 
 function Inicio() {
   return (
@@ -11,6 +12,7 @@ function Inicio() {
       <Mapa/>
       <SectionCards header="Nuestras Historias" />
       <VenDonaciones/>
+      <Subscription/>
     </>
   );
 }
